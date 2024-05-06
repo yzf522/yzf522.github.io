7936a7727f90b4fe75d52e246f0106c60b20a1ca
yzf522 <2107127731@qq.com>
2024-05-06 19:51:47 +0800

    Site updated: 2024-05-06 19:51:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/06/hello-world/"];function toRandomPost(){
+var posts=["2024/05/06/hello-world/","2024/05/06/第一次测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
