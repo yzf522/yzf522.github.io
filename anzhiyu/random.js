5000493ec8043686b247de7bd05f52cda0dc78ab
yzf522 <2107127731@qq.com>
2024-05-07 15:35:11 +0800

    Site updated: 2024-05-07 15:35:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/06/上传文章/"];function toRandomPost(){
+var posts=["2024/05/07/C++笔记和项目/","2024/05/06/关于网站/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
